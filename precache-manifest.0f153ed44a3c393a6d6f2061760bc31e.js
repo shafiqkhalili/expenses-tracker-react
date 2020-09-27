3a003e32dea8351b2a71e0498afb5528894c6ff4
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-09-27 14:09:02 +0200

    Updates

---
+++ precache-manifest.0f153ed44a3c393a6d6f2061760bc31e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fc8e4732f3e3b8156da9b4905566bd",
+    "url": "/expenses-tracker-react/index.html"
+  },
+  {
+    "revision": "669c8506d0236ce3e213",
+    "url": "/expenses-tracker-react/static/css/main.88b92223.chunk.css"
+  },
+  {
+    "revision": "41c3c80d7c050a5420d4",
+    "url": "/expenses-tracker-react/static/js/2.c63b7876.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expenses-tracker-react/static/js/2.c63b7876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669c8506d0236ce3e213",
+    "url": "/expenses-tracker-react/static/js/main.4d20cad9.chunk.js"
+  },
+  {
+    "revision": "968c6d23b0c9aa7ee0fb",
+    "url": "/expenses-tracker-react/static/js/runtime-main.958ede3e.js"
+  }
+]);
